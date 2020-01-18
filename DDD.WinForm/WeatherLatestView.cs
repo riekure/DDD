1166db90e73ae5f4ad808aff8620f6eaaef7ebb8
riekure <s2darl0uvgia24@gmail.com>
2020-01-18 23:46:13 +0900

    プロジェクト ファイルを追加します。

---
+++ DDD.WinForm/WeatherLatestView.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace DDD.WinForm
+{
+    public partial class WeatherLatestView : Form
+    {
+        private readonly string ConnectionString = @"Data Source=C:\Users\ryo\Documents\DDD\DDD.db;Version=3;";
+        public WeatherLatestView()
+        {
+            InitializeComponent();
+        }
+
+        private void LatestButton_Click(object sender, EventArgs e)
+        {
+            var sql = @"
+select DataDate,
+       Condition,
+       Temperature
+from Weather
+where AreaId = @AreaId
+order by DataDate desc
+LIMIT 1";
+
+            var dt = new DataTable();
+            using(var connection = new SQLiteConnection(ConnectionString))
+            using (var command = new SQLiteCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.AddWithValue("@AreaId", this.AreaIdTextBox.Text);
+                using (var adapter = new SQLiteDataAdapter(command))
+                {
+                    adapter.Fill(dt);
+                }
+            }
+
+            if (dt.Rows.Count > 0)
+            {
+                DataDateLabel.Text = dt.Rows[0]["DataDate"].ToString();
+                ConditionLabel.Text = dt.Rows[0]["Condition"].ToString();
+                TemperatureLabel.Text = Math.Round(Convert.ToSingle(dt.Rows[0]["Temperature"]), 2) + "℃";
+            }
+        }
+    }
+}
