2fb8a166054ebc11ed0446be0e71a5617b5ad880
riekure <s2darl0uvgia24@gmail.com>
2020-01-19 12:53:06 +0900

    11.#07-2_BADの悪いところ

--- DDD.WinForm/WeatherLatestView.cs
+++ DDD.WinForm/WeatherLatestView.cs
@@ -5,6 +5,12 @@ using System.Windows.Forms;
 
 namespace DDD.WinForm
 {
+    // BAD コードの悪いところ
+    // 1. SQL が重複する
+    // 2. SQL の接続先、タイムアウト時間をアプリケーション全体で変更する場合に追従できない（全コードをチェックして書き換えが必要）
+    // 3. データ取得後の値の加工、小数点以下2桁で丸める単位の表示
+    // 4. weather テーブルの一覧表示画面を作成する場合は、そこにも同じ加工処理を各必要がある
+
     public partial class WeatherLatestView : Form
     {
         private readonly string ConnectionString = @"Data Source=C:\Users\ryo\Documents\DDD\DDD.db;Version=3;";
