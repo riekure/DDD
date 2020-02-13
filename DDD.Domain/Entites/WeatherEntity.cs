969b5fab3746de201e0b35d4dad28f88e4f5f59b
riekure <s2darl0uvgia24@gmail.com>
2020-02-13 19:23:33 +0900

    25. #20_Entityの実装

---
+++ DDD.Domain/Entites/WeatherEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DDD.Domain.Entites
+{
+    public sealed class WeatherEntity
+    {
+        // 完全コンストラクタパターン
+        public WeatherEntity(int areaId, DateTime dataDate, int condition, float temperature)
+        {
+            AreaId = areaId;
+            DataDate = dataDate;
+            Condition = condition;
+            Temperature = temperature;
+        }
+
+        public int AreaId { get; }
+        public DateTime DataDate { get; }
+        public int Condition { get; }
+        public float Temperature { get; }
+
+        public bool IsOK()
+        {
+            if (DataDate < DateTime.Now.AddMonths(-1))
+            {
+                if (Temperature < 10)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
