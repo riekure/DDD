132422df24e9f12dae90509729e4949628cb92a4
riekure <s2darl0uvgia24@gmail.com>
2020-02-14 01:16:32 +0900

    29. #22-2_ValueObjectを値として扱う

--- DDDTest.Tests/TemperatureTest.cs
+++ DDDTest.Tests/TemperatureTest.cs
@@ -14,5 +14,24 @@ namespace DDDTest.Tests
             Assert.AreEqual(12.3f, t.Value);
             Assert.AreEqual("12.30℃", t.DisplayValue);
         }
+
+        [TestMethod]
+        public void 温度Equal()
+        {
+            var t1 = new Temperature(12.3f);
+            var t2 = new Temperature(12.3f);
+
+            Assert.AreEqual(true, t1.Equals(t2));
+        }
+
+        [TestMethod]
+        public void 温度EqualEqual()
+        {
+            var t1 = new Temperature(12.3f);
+            var t2 = new Temperature(12.3f);
+
+            Assert.AreEqual(true, t1 == t2);
+        }
+
     }
 }
