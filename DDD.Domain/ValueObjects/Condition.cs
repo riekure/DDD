bbec61413a7d62c7804d2c333aabb8d4f9cd586c
riekure <s2darl0uvgia24@gmail.com>
2020-02-15 00:34:42 +0900

    36. #25_インフラストラクチャー

--- DDD.Domain/ValueObjects/Condition.cs
+++ DDD.Domain/ValueObjects/Condition.cs
@@ -8,6 +8,11 @@ namespace DDD.Domain.ValueObjects
 {
     public sealed class Condition : ValueObject<Condition>
     {
+        public static readonly Condition None = new Condition(0);
+        public static readonly Condition Sunny = new Condition(1);
+        public static readonly Condition Cloudy = new Condition(2);
+        public static readonly Condition Rain = new Condition(3);
+
         public Condition(int value)
         {
             Value = value;
@@ -19,19 +24,16 @@ namespace DDD.Domain.ValueObjects
         {
             get
             {
-                switch (this.Value)
-                {
-                    case 1:
-                        return "晴れ";
-                    case 2:
-                        return "曇り";
-                    case 3:
-                        return "雨";
-                    default:
-                        return "不明";
+                if (this == Sunny)
+                    return "晴れ";
+                if (this == Cloudy)
+                    return "曇り";
+                if (this == Rain)
+                    return "雨";
+
+                return "不明";
                 }
             }
-        }
 
         protected override bool EqualsCore(Condition other)
         {
