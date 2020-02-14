e84138842078331d0032c0aadff86b0faf5a6ed7
riekure <s2darl0uvgia24@gmail.com>
2020-02-15 00:16:32 +0900

    34. #24-1_区分のValueObject

--- DDD.Domain/Entites/WeatherEntity.cs
+++ DDD.Domain/Entites/WeatherEntity.cs
@@ -10,13 +10,13 @@ namespace DDD.Domain.Entites
         {
             AreaId = areaId;
             DataDate = dataDate;
-            Condition = condition;
+            Condition = new Condition(condition);
             Temperature = new Temperature(temperature);
         }
 
         public int AreaId { get; }
         public DateTime DataDate { get; }
-        public int Condition { get; }
+        public Condition Condition { get; }
         public Temperature Temperature { get; }
 
         public bool IsOK()
---
+++ DDD.Domain/ValueObjects/Condition.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Domain.ValueObjects
+{
+    public sealed class Condition : ValueObject<Condition>
+    {
+        public Condition(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; }
+
+        public string DisplayValue
+        {
+            get
+            {
+                switch (this.Value)
+                {
+                    case 1:
+                        return "晴れ";
+                    case 2:
+                        return "曇り";
+                    case 3:
+                        return "雨";
+                    default:
+                        return "不明";
+                }
+            }
+        }
+
+        protected override bool EqualsCore(Condition other)
+        {
+            return this.Value == other.Value;
+        }
+    }
+}
