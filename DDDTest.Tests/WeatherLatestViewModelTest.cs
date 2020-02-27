a9492808df68a4c8b4b0f8b1f45ec0a5294622a2
riekure <s2darl0uvgia24@gmail.com>
2020-02-27 23:29:04 +0900

    39. #27_Moqの使い方

--- DDDTest.Tests/WeatherLatestViewModelTest.cs
+++ DDDTest.Tests/WeatherLatestViewModelTest.cs
@@ -4,6 +4,7 @@ using DDD.Domain.Entites;
 using DDD.Domain.Repositories;
 using DDD.WinForm.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace DDDTest.Tests
 {
@@ -13,7 +14,21 @@ namespace DDDTest.Tests
         [TestMethod]
         public void シナリオ()
         {
-            var viewModel = new WeatherLatestViewModel(new WeatherMock());
+            var weatherMock = new Mock<IWeatherRepository>();
+            weatherMock.Setup(x => x.GetLatest(1)).Returns(
+                new WeatherEntity(
+                1,
+                Convert.ToDateTime("2018/01/01 12:34:56"),
+                2,
+                12.3f));
+            weatherMock.Setup(x => x.GetLatest(2)).Returns(
+                new WeatherEntity(
+                2,
+                Convert.ToDateTime("2018/01/02 12:34:56"),
+                1,
+                22.123f));
+
+            var viewModel = new WeatherLatestViewModel(weatherMock.Object);
             Assert.AreEqual("", viewModel.AreaIdText);
             Assert.AreEqual("", viewModel.DataDateText);
             Assert.AreEqual("", viewModel.ConditionText);
@@ -25,19 +40,13 @@ namespace DDDTest.Tests
             Assert.AreEqual("2018/01/01 12:34:56", viewModel.DataDateText);
             Assert.AreEqual("曇り", viewModel.ConditionText);
             Assert.AreEqual("12.30 ℃", viewModel.TemperatureText);
-        }
-    }
 
-    internal class WeatherMock : IWeatherRepository
-    {
-        public WeatherEntity GetLatest(int areaId)
-        {
-            return new WeatherEntity(
-                1,
-                Convert.ToDateTime("2018/01/01 12:34:56"),
-                2,
-                12.3f
-                ); ;
+            viewModel.AreaIdText = "2";
+            viewModel.Search();
+            Assert.AreEqual("2", viewModel.AreaIdText);
+            Assert.AreEqual("2018/01/02 12:34:56", viewModel.DataDateText);
+            Assert.AreEqual("晴れ", viewModel.ConditionText);
+            Assert.AreEqual("22.12 ℃", viewModel.TemperatureText);
         }
     }
 }
