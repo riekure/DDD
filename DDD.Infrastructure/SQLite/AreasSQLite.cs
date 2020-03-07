c7346af6d2592167798f18c847dde6cfd72fcf46
riekure <s2darl0uvgia24@gmail.com>
2020-03-07 11:10:42 +0900

    46. #31-1_SQLiteHlperで共通化

--- DDD.Infrastructure/SQLite/AreasSQLite.cs
+++ DDD.Infrastructure/SQLite/AreasSQLite.cs
@@ -18,22 +18,12 @@ select AreaId,
        AreaName
 from Areas";
 
-            var result = new List<AreaEntity>();
-            using (var connection = new SQLiteConnection(SQLiteHelper.ConnectionString))
-            using (var command = new SQLiteCommand(sql, connection))
-            {
-                connection.Open();
-                using (var reader = command.ExecuteReader())
+            return SQLiteHelper.Query<AreaEntity>(sql, 
+                reader =>
                 {
-                    while (reader.Read())
-                    {
-                        result.Add(
-                            new AreaEntity(Convert.ToInt32(reader["AreaId"]),
-                                           Convert.ToString(reader["AreaName"])));
-                    }
-                }
-            }
-            return result.AsReadOnly();
+                    return new AreaEntity(Convert.ToInt32(reader["AreaId"]),
+                                          Convert.ToString(reader["AreaName"]));
+                });
         }
     }
 }
