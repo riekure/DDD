aa15289a085fd35edb0658ab7c047e8b67fa01d3
riekure <s2darl0uvgia24@gmail.com>
2020-03-08 11:21:53 +0900

    54. #34-2_Save画面の実装

---
+++ DDD.WinForm/ViewModels/WeatherSaveViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DDD.WinForm.ViewModels
+{
+    public class WeatherSaveViewModel : ViewModelBase
+    {
+        public WeatherSaveViewModel()
+        {
+            DataDateValue = DateTime.Now;
+        }
+        public object SelectedAreaId { get; set; }
+        public DateTime DataDateValue { get; set; }
+        public object SelectedCondition { get; set; }
+        public string TemparatureText { get; set; }
+    }
+}
---
+++ DDDTest.Tests/WeatherSaveViewModelTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DDD.WinForm.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DDDTest.Tests
+{
+    [TestClass]
+    public class WeatherSaveViewModelTest
+    {
+        [TestMethod]
+        public void 天気登録シナリオ()
+        {
+            var viewModel = new WeatherSaveViewModel();
+            viewModel.SelectedAreaId.IsNull;
+            viewModel.DataDateValue.Is(Convert.ToDateTime("2018/01/01 12:34:56"));
+            viewModel.SelectedCondition.Is(1);
+            viewModel.TemparatureText.Is("");
+        }
+    }
+}
