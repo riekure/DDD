d19b287c343eacc12d9d5d4c07d2f850ec66e390
riekure <s2darl0uvgia24@gmail.com>
2020-03-08 14:25:16 +0900

    58. #35-2_Guardクラスの追加

---
+++ DDD.Domain/Helpers/Guard.cs
@@ -0,0 +1,20 @@
+﻿using DDD.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Domain.Helpers
+{
+    public static class Guard
+    {
+        public static void IsNull(object o, string message)
+        {
+            if (o == null)
+            {
+                throw new InputException(message);
+            }
+        }
+    }
+}
--- DDD.WinForm/ViewModels/WeatherSaveViewModel.cs
+++ DDD.WinForm/ViewModels/WeatherSaveViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using DDD.Domain.Entites;
 using DDD.Domain.Exceptions;
+using DDD.Domain.Helpers;
 using DDD.Domain.Repositories;
 using DDD.Domain.ValueObjects;
 using System;
@@ -36,10 +37,7 @@ namespace DDD.WinForm.ViewModels
 
         public void Save()
         {
-            if (SelectedAreaId == null)
-            {
-                throw new InputException("エリアを選択してください");
-            }
+            Guard.IsNull(SelectedAreaId, "エリアを選択してください");
         }
     }
 }
